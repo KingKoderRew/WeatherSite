dd5f0fea6d75bdfc5f1fbec006efa6bcdc4b973d
KingKoderRew <98848738+KingKoderRew@users.noreply.github.com>
2022-05-02 14:13:21 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -17,7 +16,7 @@ app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to serve
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
